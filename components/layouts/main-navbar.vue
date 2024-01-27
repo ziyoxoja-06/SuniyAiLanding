b7b840506f78bad32237c610478e1e523279177d
ziyoxoja-06 <ziyoxojas@gmail.com>
2024-01-27 23:41:11 +0500

    Finished count

--- components/layouts/main-navbar.vue
+++ components/layouts/main-navbar.vue
@@ -18,9 +18,9 @@ onBeforeUnmount(()=>{
 })
 </script>
 
-<template>
-    <div class="overflow-hidden bg-[#1E212C] fixed top-0 left-0 right-0 bg-gray-100 dark:bg-gray-900 z-50 transition-all duration-500"
-    :class="responsive<740&&sitebar?'h-[70px] sm:h-[10vh] md:h-[10vh] xl:h-[10vh] pt-3 sm:pt-3 md:pt-0 xl:pt-0':'h-full sm:h-full md:h-[10vh] xl:h-[10vh] pt-3 sm:pt-3 md:pt-0 xl:pt-0'">
+<template class="scroll-smooth">
+    <div class="overflow-hidden bg-[#1E212C] fixed top-0 left-0 right-0 dark:bg-gray-900 z-50 transition-all duration-500"
+    :class="responsive<800&&sitebar?'h-[70px] sm:h-[80px] md:h-[10vh] xl:h-[10vh] pt-3 sm:pt-3 md:pt-0 xl:pt-0 ':'h-full sm:h-full md:h-full lg:h-[10vh] xl:h-[10vh]  pt-3 sm:pt-3 md:pt-0 xl:pt-0'">
         <div class="container  mx-auto flex flex-col sm:flex-col md:flex-row xl:flex-row items-center h-full justify-between">
             <div class="w-full sm:w-full md:w-auto xl:w-auto flex justify-between">
               <NuxtLink to="/" class="flex items-center pl-2">
@@ -53,17 +53,17 @@ onBeforeUnmount(()=>{
             </div>
             
             <div class="w-full sm:w-full md:w-auto xl:w-auto  h-full sm:h-full md:h-auto xl:h-auto flex items-center  md:space-x-4 xl:space-x-4 flex-col sm:flex-col md:flex-row xl:flex-row">
-                <NuxtLink to="/auth" class="py-4 sm:py-4 md:py-0 xl:py-0 my-1 mt-5 sm:my-2 sm:mt-5 md:my-0 xl:my-0 flex justify-center transition-all  duration-700 rounded-md w-full sm:w-full md:w-auto xl:w-auto hover:dark:bg-gray-500 sm:hover:dark:bg-gray-500 md:hover:dark:bg-gray-900 xl:hover:dark:bg-gray-900">
+                <NuxtLink to="#home" class="py-4 sm:py-4 md:py-2 xl:py-0 my-1 mt-5 sm:my-2 sm:mt-5 md:my-2 xl:my-0 flex justify-center transition-all  duration-700 rounded-md w-full sm:w-full md:w-auto xl:w-auto hover:dark:bg-gray-500 sm:hover:dark:bg-gray-500 md:hover:dark:bg-gray-900 xl:hover:dark:bg-gray-900">
                     Home 
                 </NuxtLink>
-                <NuxtLink to="/auth" class="py-4 sm:py-4 md:py-0 xl:py-0 my-1 sm:my-2 md:my-0 xl:my-0 flex justify-center transition-all  duration-700 rounded-md w-full sm:w-full md:w-auto xl:w-auto hover:dark:bg-gray-500 sm:hover:dark:bg-gray-500 md:hover:dark:bg-gray-900 xl:hover:dark:bg-gray-900">
+                <NuxtLink to="#about" class="py-4 sm:py-4 md:py-2 xl:py-0 my-1 sm:my-2 md:my-2 xl:my-0 flex justify-center transition-all  duration-700 rounded-md w-full sm:w-full md:w-auto xl:w-auto hover:dark:bg-gray-500 sm:hover:dark:bg-gray-500 md:hover:dark:bg-gray-900 xl:hover:dark:bg-gray-900">
                     About
                 </NuxtLink>
-                <NuxtLink to="/auth" class="py-4 sm:py-4 md:py-0 xl:py-0 my-1 sm:my-2 md:my-0 xl:my-0 flex justify-center transition-all  duration-700 rounded-md w-full sm:w-full md:w-auto xl:w-auto hover:dark:bg-gray-500 sm:hover:dark:bg-gray-500 md:hover:dark:bg-gray-900 xl:hover:dark:bg-gray-900">
+                <NuxtLink to="#services" class="py-4 sm:py-4 md:py-2 xl:py-0 my-1 sm:my-2 md:my-2 xl:my-0 flex justify-center transition-all  duration-700 rounded-md w-full sm:w-full md:w-auto xl:w-auto hover:dark:bg-gray-500 sm:hover:dark:bg-gray-500 md:hover:dark:bg-gray-900 xl:hover:dark:bg-gray-900">
                     Services
                 </NuxtLink>
-                <NuxtLink to="/auth" class="py-4 sm:py-4 md:py-0 xl:py-0 my-1 sm:my-2 md:my-0 xl:my-0 flex justify-center transition-all  duration-700 rounded-md w-full sm:w-full md:w-auto xl:w-auto hover:dark:bg-gray-500 sm:hover:dark:bg-gray-500 md:hover:dark:bg-gray-900 xl:hover:dark:bg-gray-900">
-                    Prising
+                <NuxtLink to="#partfoliyo" class="py-4 sm:py-4 md:py-2 xl:py-0 my-1 sm:my-2 md:my-2 xl:my-0 flex justify-center transition-all  duration-700 rounded-md w-full sm:w-full md:w-auto xl:w-auto hover:dark:bg-gray-500 sm:hover:dark:bg-gray-500 md:hover:dark:bg-gray-900 xl:hover:dark:bg-gray-900">
+                    Partfoliyo
                 </NuxtLink>
                 <SharedAnimationBtn title="Buyurtma berish" />
                 <SharedDropdown class="mt-3 sm:mt-3 md:mt-0 xl:mt-0 w-auto">
@@ -74,3 +74,8 @@ onBeforeUnmount(()=>{
         </div>
     </div>
 </template>
+<style>
+html {
+  scroll-behavior: smooth;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -51,12 +51,20 @@ const partfoliyo = [
             officiis possimus doloremque quae! Aliquid, quasi! Reprehenderit,
             impedit eius similique repudiandae facere a excepturi quas totam
             nulla amet est provident maxime?`,
+    img:'/partfoliyo_2.jpg',
+  },
+  {
+    title:'Suniy TV',
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            officiis possimus doloremque quae! Aliquid, quasi! Reprehenderit,
+            impedit eius similique repudiandae facere a excepturi quas totam
+            nulla amet est provident maxime?`,
     img:'/partfoliyo_3.jpeg',
   },
 ]
 
 function strCut(count:number, text:string='' ) {  
-  return count? text.split(' ').slice(0, count).join(' '):text
+  return count? text.split(' ').slice(0, count).join(' ')+'...':text
 }
 </script>
 
@@ -64,13 +72,13 @@ function strCut(count:number, text:string='' ) {
   <!-- HERO SECTION  -->
   <section
     id="home"
-    class="flex items-center justify-between flex-col sm:flex-col md:flex-row xl:flex-row min-h-[100vh] pt-[10vh] gap-4 bg-[#1E212C] bg-[url('baseBg.svg')]"
+    class="flex items-center flex-col sm:flex-col md:flex-row xl:flex-row min-h-[100vh] pt-[10vh] gap-4 bg-[#1E212C] bg-[url('baseBg.svg')]"
   >
     <div
-      class="px-4 sm:pl-4 md:pl-20 xl:pl-20 pt-20 sm:pt-20 md:pt-0 xl:pt-0 flex flex-col space-y-3 w-[100vw] sm:w-[100vw] md:w-[50%] xl:w-[50%]"
+      class="px-4 sm:pl-2 md:pl-10 xl:pl-10 pt-20 sm:pt-20 md:pt-0 xl:pt-0 flex flex-col space-y-3 w-[100vw] sm:w-[100vw] md:w-[50%] xl:w-[50%]"
     >
       <h1
-        class="text-white text-[34px] sm:text-[34px] md:text-[46px] xl:text-[46px] leading-[36px] sm:leading-[46px] md:leading-[56px] xl:leading-[56px] font-medium font-['Overpass'] mt-10"
+        class="home__title text-white text-[34px] sm:text-[34px] md:text-[46px] xl:text-[46px] leading-[36px] sm:leading-[46px] md:leading-[56px] xl:leading-[56px] font-medium font-['Overpass'] mt-10"
       >
         Orzularingiz orzuligicha qolmaydi. Endi
         <span
@@ -87,7 +95,7 @@ function strCut(count:number, text:string='' ) {
         qurish uchun asos sifatida ishlatamiz.
       </div>
       <div
-        class="w-[90vw] sm:w-[90vw] md:w-[50%] xl:w-[50%] h-0.5 bg-gradient-to-r from-cyan-400 to-cyan-800"
+        class="w-[90vw] sm:w-[90vw] md:w-[40vw] xl:w-[40vw] h-0.5 bg-gradient-to-r from-cyan-400 to-cyan-800"
       ></div>
       <div class="pt-[10px] flex space-x-4">
         <BorderedBtn :title="'Portfolio'" />
@@ -96,14 +104,14 @@ function strCut(count:number, text:string='' ) {
     </div>
     <NuxtImg
       src="/HomeHande.svg"
-      class="w-[100vw] sm:w-[100vw] md:w-[50%] xl:w-[50%] mt-5"
+      class="w-[100vw] sm:w-[100vw] md:w-[50%] xl:w-[50%] mt-10"
     />
   </section>
   <section
     id="about"
     class="flex items-center  flex-col sm:flex-col md:flex-row xl:flex-row min-h-[100vh] pt-[10vh] gap-4 bg-[#1E212C]"
   >
-  <div class="flex justify-end ml-1 sm:ml-1 md:ml-8 xl:ml-8 w-[90vw] sm:w-[90vw] md:w-[50%] xl:w-[50%] ">
+  <div class="flex md:justify-end lg:justify-end xl:justify-end ml-1 sm:ml-1 md:ml-8 xl:ml-8 w-[90vw] sm:w-[90vw] md:w-[50%] xl:w-[50%] ">
     <NuxtImg
       src="/about.png"
       class="w-[90%] h-[450px]"
@@ -168,19 +176,22 @@ function strCut(count:number, text:string='' ) {
   </section>
   <section
     id="partfoliyo"
-    class="p-2 sm:p-2 md:p-12 xl:p-12 bg-[#1E212C] h-fit"
+    class="p-2 sm:p-2 md:p-4 xl:p-4 py-2 md:py-12 lg:py-12 xl:py-12 bg-[#1E212C] h-fit"
   >
+  <!-- Card group -->
     <div
       class="text-white text-center py-12 sm:py-12 md:py-6 xl:py-6 text-[40px] font-semibold font-['Overpass']"
     >
       Bizning xizmatlar
     </div>
     <div
-      class=" flex justify-center flex-wrap space-x-2 space-y-4 sm:space-y-4 md:space-y-0 xl:space-y-0 px-2 sm:px-2 md:px-10 xl:10 "
+      class=" flex justify-center flex-wrap"
     >
+
+  <!-- Card -->
       <div
       v-for="item in partfoliyo" :key="item.title"
-        class="w-[300px] rounded-xl overflow-hidden border border-gray-600"
+        class="card w-[264px] relative rounded-xl overflow-hidden border m-3 border-gray-600"
       >
       <div class="h-[200px]">
         <NuxtImg :src="item.img" class="h-full"/>
@@ -188,10 +199,53 @@ function strCut(count:number, text:string='' ) {
         <div class="p-4">
           <h2 class="font-bold text-xl pb-2">{{ item.title }}</h2>
           <p class="text-gray-300 text-justify ">
-            {{ strCut(40,item.description) }}
+            {{ strCut(35,item.description) }}
           </p>
+        </div>
+      </div>
+      <div class="w-full p-2 sm:p-2 md:p-4 xl:p-12 py-2 md:py-12 lg:py-12 xl:py-12 px-2 md:px-24 lg:px-24 xl:px-24 bg-[#1E212C] h-fit">
+        <div class="w-full h-[259px] flex bg-gray-800 rounded-[20px] border border-teal-400 py-2 md:py-10 lg:py-10 xl:py-10 px-2 md:px-16 lg:px-16 xl:px-16">
+         <div class="w-[58%]">
+          <h2 class=" text-white text-[40px] font-semibold font-['Overpass']">Siz ham ushbu jamoa orasida bo'lishingiz mumkin...</h2>
+          <div class=" text-white text-xl font-normal font-['Inter'] leading-relaxed">Biz bilan o'sishni xohlaysizmi? Endi sizga kerakligini isbotlang! Jamoaning ajralmas qismiga aylaning</div>
+         </div>
+         <div class="w-[42%] flex flex-col space-y-4 justify-center items-center ml-24">
+          <div class="flex items-center space-x-2">
+            <div class="w-[50px] h-0.5 bg-zinc-300"></div>
+            <div class="text-white text-xl font-semibold font-['Inter']">Big data</div>
+          </div>
+          <div class="flex items-center space-x-2">
+            <div class="w-[50px] h-0.5 bg-zinc-300"></div>
+            <div class="text-white text-xl font-semibold font-['Inter']">Ved development</div>
+          </div>
+          <div class="flex items-center space-x-2">
+            <div class="w-[50px] h-0.5 bg-zinc-300"></div>
+            <div class="text-white text-xl font-semibold font-['Inter']">AI ingineering</div>
+          </div>
+          <div class="flex items-center space-x-2">
+            <div class="w-[50px] h-0.5 bg-zinc-300"></div>
+            <div class="text-white text-xl font-semibold font-['Inter']">Android & IOS</div>
+          </div>
+         </div>
         </div>
       </div>
     </div>
   </section>
+  <section
+    id="footer"
+    class="  flex-col sm:flex-col md:flex-row xl:flex-row min-h-[100vh] pt-[10vh] gap-4 bg-[#1E212C]"
+  >
+  
+  </section>
 </template>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bangers&family=Inter:wght@500&family=Overpass&family=Space+Grotesk:wght@500&display=swap');
+
+.home__title{
+  font-family: 'Bangers', system-ui;
+font-family: 'Inter', sans-serif;
+font-family: 'Overpass', sans-serif;
+font-family: 'Space Grotesk', sans-serif;
+}
+
+</style>
